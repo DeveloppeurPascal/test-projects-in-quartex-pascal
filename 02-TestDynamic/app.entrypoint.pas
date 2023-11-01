83172f84d15c8b7c6cd9cf0762b585a0d9cfba4b
Patrick PREMARTIN <patrick.premartin@developpeur-pascal.fr>
2023-11-01 18:51:35 +0100

    simple test in a dynamic form project

---
+++ 02-TestDynamic/app.entrypoint.pas
@@ -0,0 +1,21 @@
+﻿uses
+  qtx.sysutils,
+  qtx.classes,
+  qtx.time,
+  qtx.dom.widgets,
+  qtx.dom.theme,
+  qtx.dom.types,
+  qtx.dom.events,
+  qtx.dom.graphics,
+  qtx.dom.application, 
+  qtx.dom.forms,
+form1;
+                       
+begin
+  TQTXDOMApplicationFull.Create(nil, procedure (Widget: TQTXComponent)
+  begin
+    Widget.Handle.style["font-family"] := "Segoe UI"; 
+    {$I "app::form.init"}
+  end);
+  
+end.
---
+++ 02-TestDynamic/forms/form1.pas
@@ -0,0 +1,67 @@
+﻿unit form1;
+
+interface
+
+uses
+  qtx.sysutils,
+  qtx.classes,
+  qtx.dom.types,
+  qtx.dom.events,
+  qtx.dom.graphics,
+  qtx.dom.widgets,
+  qtx.dom.theme,
+  qtx.dom.application,
+  qtx.dom.forms,
+qtx.delegates,
+qtx.dom.events.mouse,
+qtx.dom.events.pointer,
+qtx.dom.events.keyboard,
+qtx.dom.events.touch,
+qtx.dom.stylesheet,
+qtx.dom.control.edit,
+qtx.dom.control.Button,
+qtx.dom.control.label,
+qtx.dom.control.contentbox,
+qtx.dom.control.listbox;
+
+type
+
+  Tform1 = class(TQTXForm)
+  {$I "intf::form1"}
+  protected
+    procedure InitializeObject; override;
+    procedure HandlebtnAddTextToList(Sender: TQTXDOMMouseDelegate; EventObj: JMouseEvent);
+    procedure FinalizeObject; override;
+    procedure StyleObject; override;
+  public
+
+  end;
+
+implementation
+
+procedure Tform1.InitializeObject;
+begin
+  inherited;
+  {$I "impl::form1"}
+end;
+
+procedure Tform1.HandlebtnAddTextToList(Sender: TQTXDOMMouseDelegate; EventObj: JMouseEvent);
+begin
+  if (edit1.Text.Trim.Length>0) then
+    ListBox1.AddText(edit1.Text.trim);
+end;
+
+procedure Tform1.FinalizeObject;
+begin
+  inherited;
+end;
+
+procedure Tform1.StyleObject;
+begin
+  inherited;
+  PositionMode := TQTXWidgetPositionMode.cpInitial;
+  DisplayMode := TQTXWidgetDisplayMode.cdBlock;
+end;
+
+
+end.
